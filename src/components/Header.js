2304fbca2fd22014d05b6efa62f5920dc4ba8936
wise-guru <wise.guru123@gmail.com>
2022-08-02 15:36:03 -0400

    Add header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/App.css'
+
+const Header = () => {
+        return(
+            <header className='header'>
+                <div>CV Builder</div>
+            </header>
+        )
+} 
+
+export default Header;
