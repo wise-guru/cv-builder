9cfcd4321f702c5e225a63815126142e2732ae59
wise-guru <wise.guru123@gmail.com>
2022-08-10 23:11:22 -0400

    Format

--- src/components/CVSections/Education.js
+++ src/components/CVSections/Education.js
@@ -1,23 +1,48 @@
-import React, {component} from "react";
+import React from 'react';
 
-class Education extends component {
-    render() {
-        return(
-            <section className="education">
-                <label htmlFor="programInput">Program or Degree Name</label>
-                <input type="text" id="programInput"></input>
+function Education(props) {
+  return (
+    <section className="education">
+      <h2>Education</h2>
+      <div className="infoFields">
+        <label htmlFor="programInput" />
+        <input
+          type="text"
+          id="programInput"
+          className="firstCol firstRow"
+          placeholder="Degree or Program Name"
+        />
 
-                <label htmlFor="institutionInput">Institution Name</label>
-                <input type="text" id="institutionInput"></input>
-              
-                <label htmlFor="eduStartDate">Start</label>
-                <input type="text" id="eduStartDate"></input>
+        <label htmlFor="institutionInput" />
+        <input
+          type="text"
+          id="institutionInput"
+          className="secondCol firstRow"
+          placeholder="Institution Name"
+        />
 
-                <label htmlFor="eduEndDate">End</label>
-                <input type="text" id="eduEndDate"></input>
-            </section>
-        )
-    }
+        <label htmlFor="eduStartDate" />
+        <input
+          type="text"
+          id="eduStartDate"
+          className="firstCol secondRow"
+          placeholder="Start Date"
+        />
+
+        <label htmlFor="eduEndDate" />
+        <input
+          type="text"
+          id="eduEndDate"
+          className="secondCol secondRow"
+          placeholder="End Date"
+        />
+      </div>
+      <div className="formButtons">
+        <button className="add">Add Education</button>
+        <button className="delete">Delete</button>
+      </div>
+    </section>
+  );
 }
 
-export default Education
+export default Education;
