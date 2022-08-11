7161bf82472326ff6edbff0c4f6b32c0b4fadf86
wise-guru <wise.guru123@gmail.com>
2022-08-10 23:17:21 -0400

    Add skill input

--- src/components/CVSections/Skills.js
+++ src/components/CVSections/Skills.js
@@ -1,32 +1,57 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
+import uniqid from 'uniqid';
+import SkillOverview from '../SkillOverview';
 
-class Skills extends Component() {
+class Skills extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super()
-        
-        this.state = {
-            skill: '',
-            skills: []
-        }
-    }
+    this.state = {
+      skill: {
+        text: '',
+        id: uniqid(),
+      },
+      skills: [],
+    };
+  }
 
-    onSkillSubmit = (e) => {
-        e.preventDefault()
-        this.setState({
-            skills: this.state.skills.concat(this.state.skill),
-            skill: ''
-        })
-    }
-    
-    render() {
-        <section>
-            <label htmlFor="skillsInput"></label>
-            <input type="text" id="skillsInput"></input>
+  onSkillSave = e => {
+    e.preventDefault();
+    this.setState({
+      skill: {
+        text: e.target.value,
+      },
+      skills: this.state.skills.concat(this.state.skill),
+    });
+  };
 
-            <button onSubmit={this.onSkillSubmit}>Add Skill +</button>
-        </section>
-    }
+  handleChange = e => {
+    this.setState({
+      skill: {
+        text: e.target.value,
+      },
+      skills: this.state.skills.replace(this.state.skill),
+    });
+  };
+
+  render() {
+    const { skill, skills } = this.state;
+    return (
+      <section>
+        <h2>Skills</h2>
+        <label htmlFor="skillsInput" />
+        <input
+          type="text"
+          id="skillsInput"
+          value={skill.text}
+          onChange={this.handleChange}
+        />
+        <button>Delete</button>
+        <button onSubmit={this.onSkillSave}>Add Skill +</button>
+        <SkillOverview skills={skills} />
+      </section>
+    );
+  }
 }
 
-export default Skills
+export default Skills;
