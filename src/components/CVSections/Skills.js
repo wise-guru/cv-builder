4d7353eb533853756e0d39ae8659eb4ef984f8fa
wise-guru <wise.guru123@gmail.com>
2022-08-13 19:23:41 -0400

    Change to functional component

--- src/components/CVSections/Skills.js
+++ src/components/CVSections/Skills.js
@@ -2,56 +2,18 @@ import React, { Component } from 'react';
 import uniqid from 'uniqid';
 import SkillOverview from '../SkillOverview';
 
-class Skills extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      skill: {
-        text: '',
-        id: uniqid(),
-      },
-      skills: [],
-    };
-  }
-
-  onSkillSave = e => {
-    e.preventDefault();
-    this.setState({
-      skill: {
-        text: e.target.value,
-      },
-      skills: this.state.skills.concat(this.state.skill),
-    });
-  };
-
-  handleChange = e => {
-    this.setState({
-      skill: {
-        text: e.target.value,
-      },
-      skills: this.state.skills.replace(this.state.skill),
-    });
-  };
-
-  render() {
-    const { skill, skills } = this.state;
-    return (
-      <section>
-        <h2>Skills</h2>
-        <label htmlFor="skillsInput" />
-        <input
-          type="text"
-          id="skillsInput"
-          value={skill.text}
-          onChange={this.handleChange}
-        />
-        <button>Delete</button>
-        <button onSubmit={this.onSkillSave}>Add Skill +</button>
-        <SkillOverview skills={skills} />
-      </section>
-    );
-  }
+function Skills(props) {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <input
+        type="text"
+        id="skillsInput"
+        value={props.skillInfo.skill}
+        onChange={props.skillHandler}
+      />
+    </section>
+  );
 }
 
 export default Skills;
