57010743404041d781ae0d3bd73a3c5dcc98dbfb
wise-guru <wise.guru123@gmail.com>
2022-08-15 22:08:56 -0400

    Remove commented code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../styles/App.css'
+import React from 'react';
+import '../styles/App.css';
 
-const Header = () => {
-        return(
-            <header className='header'>
-                <h1>CV Builder</h1>
-            </header>
-        )
-} 
+function Header() {
+  return (
+    <header className="header">
+      <h1>CV Builder</h1>
+    </header>
+  );
+}
 
 export default Header;
