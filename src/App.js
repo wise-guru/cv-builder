c769702f31cbab47a1447996bc91c2353adfd810
wise-guru <wise.guru123@gmail.com>
2022-08-15 22:14:26 -0400

    remove commented code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './styles/App.css';
 import Header from './components/Header';
+import Overview from './components/Overview';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
   render() {
     return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <div>CV Builder</div>
-    //     </header>
-    //   </div>
-    <Header />
+      <div className="container">
+        <Header />
+        <Overview />
+      </div>
     );
   }
-
 }
 
 export default App;
