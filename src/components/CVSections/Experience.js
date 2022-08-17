5bb4f10dd3c28e6674b62e29560ed778fc66eb89
wise-guru <wise.guru123@gmail.com>
2022-08-17 00:34:17 -0400

    Add counter for input fields

--- src/components/CVSections/Experience.js
+++ src/components/CVSections/Experience.js
@@ -2,47 +2,51 @@ import React from 'react';
 
 function Experience(props) {
   return (
-    <div className="infoFields">
-      <input
-        // id="companyInput"
-        className="firstCol firstRow"
-        placeholder="Company Name"
-        value={props.experienceInfo.company}
-        onChange={props.companyHandler}
-      />
+    <section>
+      <div className="counter">Experience {props.counter}</div>
+      <div className="experience infoFields">
+        <input
+          // id="companyInput"
+          className="firstCol firstRow"
+          placeholder="Company Name"
+          value={props.experienceInfo.company}
+          onChange={props.companyHandler}
+        />
 
-      <input
-        // id="positonInput"
-        className="secondCol firstRow"
-        placeholder="Position"
-        value={props.experienceInfo.position}
-        onChange={props.positionHandler}
-      />
+        <input
+          // id="positonInput"
+          className="secondCol firstRow"
+          placeholder="Position"
+          value={props.experienceInfo.position}
+          onChange={props.positionHandler}
+        />
 
-      <input
-        // id="expStartDate"
-        className="firstCol secondRow"
-        placeholder="Start Date"
-        value={props.experienceInfo.startDate}
-        onChange={props.expStartHandler}
-      />
+        <input
+          // id="expStartDate"
+          className="firstCol secondRow"
+          placeholder="Start Date"
+          value={props.experienceInfo.startDate}
+          onChange={props.expStartHandler}
+        />
 
-      <input
-        // id="expEndDate"
-        className="secondCol secondRow"
-        placeholder="End Date"
-        value={props.experienceInfo.endDate}
-        onChange={props.expEndHandler}
-      />
+        <input
+          // id="expEndDate"
+          className="secondCol secondRow"
+          placeholder="End Date"
+          value={props.experienceInfo.endDate}
+          onChange={props.expEndHandler}
+        />
 
-      <textarea
-        // id="jobDescription"
-        className="bothCol thirdRow"
-        placeholder="Job Description"
-        value={props.experienceInfo.description}
-        onChange={props.expDescriptionHandler}
-      />
-    </div>
+        <textarea
+          // id="jobDescription"
+          className="bothCol thirdRow"
+          placeholder="Job Description"
+          value={props.experienceInfo.description}
+          onChange={props.expDescriptionHandler}
+          rows={4}
+        />
+      </div>
+    </section>
   );
 }
 
