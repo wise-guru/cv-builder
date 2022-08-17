15a4147360f82f050a56e27b5c4aec66549bc012
wise-guru <wise.guru123@gmail.com>
2022-08-17 00:39:32 -0400

    add class for styling

--- src/components/CVSections/Skills.js
+++ src/components/CVSections/Skills.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 function Skills(props) {
   return (
-    <section>
-      <h2>Skills</h2>
+    <section className="skills infoFields">
       <input
         type="text"
         className="skillsInput"
